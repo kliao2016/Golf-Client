63f861c86ea16df098e0f5c49116a0e17667ab50
Kevin Liao <kliao2016@gmail.com>
2018-07-08 12:30:23 -0600

    Initial commit with README

---
+++ PGA Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PGA Client
+//
+//  Created by Kevin Liao on 7/7/18.
+//  Copyright © 2018 Kevin Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+A PGA Tour mobile client that allows you to track players in the current
+tournament being played and notifies you when that player makes a certain score
+on a hole
